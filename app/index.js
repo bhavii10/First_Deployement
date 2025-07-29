54fb619a3c9768957322d5e02f22bbdbb6cc5c87
Bhavisha Ahuja <ahuja.bhavisha@gmail.com>
2025-07-29 20:39:32 +0530

    My last deploy

--- app/index.js
+++ app/index.js
@@ -1,9 +1,11 @@
 const express=require('express');
 const app=express();
-const port=8083;
+// const port=8083;
 const ejs= require("ejs");
-
+const path=require("path");
 app.set("view engine","ejs");
+
+app.set("views",path.join(__dirname,"views"));
 
 
 // app.use(express.static("public"));   middleware
@@ -14,3 +16,5 @@ app.listen(port,()=>{
 app.get("/",(req,res)=>{
     res.render("index");
 })
+
+module.exports=app;
